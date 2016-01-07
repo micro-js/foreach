171b418244973459b5cb69df1beea33d4e234598
Andrew Shaffer <andrew@weo.io>
2016-01-07 11:47:47 -0800

    update to use foreach-array and foreach-obj so that its more performant in the array case

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
  * Imports
  */
 
-var foreach = require('..')
+var forEach = require('..')
 var test = require('tape')
 
 /**
@@ -13,10 +13,19 @@ test('should iterate over object', function (t) {
   var obj = {1: 1, 2: 2, 3: 3}
   var clone = {}
 
-  foreach(function (val, key) {
+  forEach(function (val, key) {
     clone[key] = val
   }, obj)
 
   t.deepEqual(obj, clone)
   t.end()
+})
+
+test('should iterate over array', function (t) {
+  t.plan(2)
+
+  forEach(function (val, idx) {
+    t.equal(val, 2)
+    t.equal(idx, 0)
+  }, [2])
 })
